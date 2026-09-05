dfb1e22152283ef2b089e342aa4f8b74c363f3ee
lxpzdylib41-web <lxpzdylib41@gmail.com>
2026-09-05 14:32:24 -0700

    Update snipercodesx23.lua

--- snipercodesx23.lua
+++ snipercodesx23.lua
@@ -2,9 +2,9 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local playerGui = player.PlayerGui
-local PRODUCT_ID = "x23-clone"
+local PRODUCT_ID = "x23-Premium Edition"
 local API_URL = "https://0df0d4ce-f84e-423b-971d-a243eea7e01f-00-1pzapgoozo9mk.picard.replit.dev/api/keys/validate"
-local HUB_URL = "https://github.com/lxpzdylib41-web/snipercodesasmmyupdate/blob/main/cookiesxacecodetyper.lua"
+local HUB_URL = "https://github.com/lxpzdylib41-web/snipercodesasmmyupdate/blob/main/cookiesxacecodetyper.lua
 
 local function getHWID()
     local ok, value = pcall(function()
